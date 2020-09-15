c0d3303192322a124f4eee6e6cf972376b0a0510
01010111 <will.blanton.804@gmail.com>
2020-09-15 12:59:40 -0400

    remove controls shortcuts

--- src/Controls.hx
+++ src/Controls.hx
@@ -1,13 +1,6 @@
 #if keep @:keep #end
 class Controls {
-	// Gamepad shortcuts
-	public static var A = 88;
-	public static var B = 67;
-	public static var U = 38;
-	public static var D = 40;
-	public static var L = 37;
-	public static var R = 39;
-	public static var MOUSE = -1;
+
 	// Mouse position
 	public static var M = {x:0,y:0}
 	
@@ -20,8 +13,8 @@ class Controls {
 	public static function init() {
 		document.addEventListener('keydown', (e) -> k.set(e.keyCode, true));
 		document.addEventListener('keyup', (e) -> k.set(e.keyCode, false));
-		CTX.canvas.onpointerdown = (e) -> k.set(-1,true);
-		CTX.canvas.onpointerup = (e) -> k.set(-1,false);
+		CTX.canvas.onpointerdown = (e) -> k.set(-1 - e.button, true);
+		CTX.canvas.onpointerup = (e) -> k.set(-1 - e.button, false);
 		CTX.canvas.onpointermove = (e) -> M = {
 			x: (e.offsetX / Game.zx).floor(),
 			y: (e.offsetY / Game.zy).floor()
